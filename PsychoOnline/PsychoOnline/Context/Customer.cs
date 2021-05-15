74694538c4b964e8968cddbd9eb41e871e35316b
mdzhura <32909428+mdzhura@users.noreply.github.com>
2021-05-15 22:24:35 +0300

    Created "Context" and "Migration"

---
+++ PsychoOnline/PsychoOnline/Context/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PsychoWeb.Context
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+    }
+}
